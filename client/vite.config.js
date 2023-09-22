9e0ae5e23416d7b614fe08e85eb02868849d7821
tomer <tomertal52@yahoo.com>
2023-09-22 11:54:42 -0400

    created new branch

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// const API_ENDPOINT = process.env.VITE_API_ENDPOINT
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
